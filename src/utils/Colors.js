575bd188b1af4367467b626ed611c748eb00477b
Ilya <ilyaphotoshop17@gmail.com>
2020-04-19 12:22:34 +0300

    [add] create utility/colors components

---
+++ src/utils/Colors.js
@@ -0,0 +1,10 @@
+import color = {
+    black: '#000009',
+    theme: '#232323',
+    charcoal: '#474747',
+    gray: '#7d7d7d',
+    lightisgray: '#9d9d9d',
+    smoke: '#EEEEEE',
+    white: '#FFFFFF',
+    logocolor: '#219653'
+}

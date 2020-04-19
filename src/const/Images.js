df3710057579d41b6908bdcffaa333874572053d
Ilya <ilyaphotoshop17@gmail.com>
2020-04-19 12:46:12 +0300

    [add] create dir/assets and create Images/String components

---
+++ src/const/Images.js
@@ -0,0 +1,5 @@
+const images = {
+    logo: require('../../assets/wiseLogo.png')
+}
+
+export default images
---
+++ src/const/String.js
@@ -0,0 +1,9 @@
+const String = {
+    Join: 'Join Now',
+    InvalidEmailAddress: 'Invalid Email Address',
+    PasswordFieldEmpty: 'Password Field',
+    EmailPlaceHolder: 'Email Address',
+    PasswordPlaceHolder: 'Password'
+}
+
+export default String

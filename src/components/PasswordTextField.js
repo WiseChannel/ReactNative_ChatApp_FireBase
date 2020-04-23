ddc7508c3571ed65293b807deff93d789049ac8e
Ilya <ilyaphotoshop17@gmail.com>
2020-04-23 19:59:07 +0300

    [add] create PasswordTextField component

---
+++ src/components/PasswordTextField.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { TextInput, Text, View, StyleSheet } from 'react-native'
+
+//import components
+import Utility from "../utils/Utility";
+import Colors from "../utils/Colors";
+import String from "../const/String";
+import Constants from "../const/Constants";
+
+const PasswordTextField = ({ term, placeHolder, OnTermChange, onValidatePasswordField, error }) => {
+    return (
+        <View>
+            <Text style={style.ErrorText}>{error}</Text>
+            <Text style={style.TextField}>
+                <TextInput
+                    autoCorrect={false}
+                    secureTextEntry={}
+                    style={style.TextField}
+                    placeholder={placeHolder}
+                    value={term}
+                    onChangeText={OnTermChange}
+                    onEndEditing={onValidatePasswordField}
+                />
+            </Text>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+    TextField: {
+        fontSize: 14,
+        flex: 1,
+        marginHorizontal: 20
+    },
+    TextFieldView: {
+        height: Constants.screenHeight * 0.06,
+        weight: Constants.screenWidth * 0.85,
+        borderRadius: 10,
+        marginBottom: 10,
+        marginTop: 5,
+        borderColor: Colors.black,
+        borderWidth: 1,
+        justifyContent: 'center',
+        backgroundColor: Colors.smoke
+    },
+    ErrorText: {
+        fontSize: 12,
+        color: Colors.red,
+        marginBottom: -5,
+        marginHorizontal: 20
+    }
+})
+
+export default PasswordTextField

bd9dd36a29dd58ce5c5b2e3c488d3cdcb1527e70
Ilya <ilyaphotoshop17@gmail.com>
2020-04-26 21:08:11 +0300

    [add] add directory firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAboZTZx_7qrREPeRU5u9lgHHizAIFxYmc",
+    authDomain: "reactnativechat-52fc4.firebaseapp.com",
+    databaseURL: "https://reactnativechat-52fc4.firebaseio.com",
+    projectId: "reactnativechat-52fc4",
+    storageBucket: "reactnativechat-52fc4.appspot.com",
+    messagingSenderId: "48135712910",
+    appId: "1:48135712910:web:4ce730047627852ede99df",
+    measurementId: "G-F83VMREG7F"
+  };

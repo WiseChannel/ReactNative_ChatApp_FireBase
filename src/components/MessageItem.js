685f86502fb5e6346f423aa2e24ad15136bb6dcb
Ilya <ilyaphotoshop17@gmail.com>
2020-04-28 15:29:22 +0300

    [add] implementing message component

---
+++ src/components/MessageItem.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import {StyleSheet, Text, View, Image } from 'react-native'
+import Constants from "../const/Constants";
+import Images from '../const/Images'
+import Colors from '../utils/Colors'
+import firebase, {firestore} from "firebase";
+
+function MessageItem({ item }) {
+    const userID = firebase.auth().currentUser.uid
+
+    function messageView() {
+        if(userID === item.senderId()) {
+            return (
+                <View style={styles.otherMessageContainerView}>
+                    <Text
+                        style={[styles.senderName, { textAlign: 'right' }]} >
+                        {item.senderEmail}
+                    </Text>
+                    <Text
+                        style={[styles.message(), { textAlign: 'right' }]} >
+                        {item.message}
+                    </Text>
+                </View>
+            )
+        } else {
+            return(
+                <View style={styles.myMessageContainerView}>
+                    <Text style={styles.senderName}>
+                        {item.senderName}
+                    </Text>
+                    <Text style={styles.message()}>
+                        {item.message}
+                    </Text>
+                </View>
+            )
+        }
+    }
+}
+
+const styles = StyleSheet.create({
+    otherMessageContainerView: {
+        width: Constants.screenWidth - 50,
+        backgroundColor: Colors.gray,
+        borderRadius: 5,
+        marginLeft: 25,
+        marginTop: 5,
+        marginBottom: 5,
+        padding: 10
+    },
+    myMessageContainerView: {
+        width: Constants.screenWidth - 50,
+        backgroundColor: Colors.gray,
+        borderRadius: 5,
+        margin: 5,
+        padding: 10
+    },
+    senderName: {
+        color: Colors.white,
+        fontSize: 14,
+        fontWeight: 'bold'
+    },
+    message: {
+        color: Colors.white,
+        fontSize: 14
+    }
+})
+
+export default MessageItem
